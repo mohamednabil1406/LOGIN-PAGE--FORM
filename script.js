e0b232913da06490a91a19b174124df422d7a297
mohamednabil1406 <connectwithmhd@gmail.com>
2025-08-16 21:32:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Save login state in localStorage
+
+function loginUser(event) {
+  event.preventDefault();
+  localStorage.setItem("isLoggedIn", "true");
+  window.location.href = "home.html"; 
+}
+
+function signupUser(event) {
+  event.preventDefault();
+  localStorage.setItem("isLoggedIn", "true");
+  window.location.href = "home.html"; 
+}
+
+function checkLogin() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (!isLoggedIn || isLoggedIn !== "true") {
+    window.location.href = "login.html"; 
+  }
+}
+
+function logoutUser() {
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "login.html";
+}
